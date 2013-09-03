663670f4fe59b5e938cf8fd05d2ce258ae33a03a
becky-zoovy <becky@zoovy.com>
2013-09-03 14:20:49 -0700

    commit 0903

---
+++ NodeProxy/CA.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenSSL;
+using OpenSSL.Core;
+using OpenSSL.Crypto; 
+using OpenSSL.X509; 
+
+namespace NodeProxy
+{
+    class CA
+    {
+        public static CryptoKey CreateNewRSAKey(int numberOfBits)
+        {
+            using (var rsa = new RSA())
+            {
+                BigNumber exponent = 0x10001; // this needs to be a prime number
+                rsa.GenerateKeys(numberOfBits, exponent, null, null);
+
+                return new CryptoKey(rsa);
+            }
+        }
+        
+        public X509Request CreateCertificateSigningRequest()
+        {
+            using (var requestDetails = GetCertificateSigningRequestSubject())
+            using (var key = CreateNewRSAKey(4096))
+            {
+                int version = 2; // Version 2 is X.509 Version 3
+                return new X509Request(version, requestDetails, key);
+            }
+        }
+
+        private static X509Name GetCertificateSigningRequestSubject()
+        {
+            var requestDetails = new X509Name();
+
+            requestDetails.Common = "http://testserver.example.com/";
+            requestDetails.Country = "UK";
+            requestDetails.StateOrProvince = "Hampshire";
+            requestDetails.Organization = "Test Co";
+            requestDetails.OrganizationUnit = "Development";
+
+            return requestDetails;
+        }
+         
+    }
+}
--- NodeProxy/Form1.cs
+++ NodeProxy/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using OpenSSL.X509;
+using System.IO;
 
 namespace NodeProxy
 {
@@ -14,6 +16,35 @@ namespace NodeProxy
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var signedCert = new X509Request();
+
+            var Test = new CA();
+            signedCert = Test.CreateCertificateSigningRequest();
+
+            // Now you can save this certificate to your file system.
+            FileStream fs = new FileStream(@"C:\Users\Becky\Documents\zoovy\NodeProxy\new_cert.cer", FileMode.Create, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(fs);
+            OpenSSL.Core.BIO bio = OpenSSL.Core.BIO.MemoryBuffer();
+            signedCert.Write(bio);
+            string certString = bio.ReadString();
+            bw.Write(certString);
+
+            bw.Close();
+            bio.Dispose();
+
+            //using (var bio = OpenSSL.Core.BIO.File(@"C:\new cert.cer", "w"))
+            //{
+            //    signedCert.Write(bio);
+            //}
+
+            
+
+            //var signedCert = CA.ProcessRequest(x509Request, DateTime.UtcNow,
+            //DateTime.UtcNow.AddYears(1), MessageDigest.SHA512);
         }
     }
 }
